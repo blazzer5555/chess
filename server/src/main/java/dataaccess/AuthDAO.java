b52716c123226801546323e66c643d419aaec27b
blazzer <blazzer@students.byu.edu>
2026-03-02 13:09:18 -0700

    Created Auth and User DAOs

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import model.*;
+import model.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class AuthDAO {
+
+    Set<AuthData> setOfAuthTokens = new HashSet<>();
+    Map<String, AuthData> authTokenToAuthData = new HashMap<>();
+
+    AuthData getAuth(String authToken) {
+        return authTokenToAuthData.getOrDefault(authToken, null);
+    }
+
+    void createAuth(AuthData authData) {
+        setOfAuthTokens.add(authData);
+        authTokenToAuthData.put(authData.authToken(), authData);
+    }
+
+    void clear() {
+        setOfAuthTokens.clear();
+        authTokenToAuthData.clear();
+    }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import model.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
 public class UserDAO {
+
+    Set<UserData> setOfUsers = new HashSet<>();
+    Map<String, UserData> usernameToUserData = new HashMap<>();
+
+    UserData getUser(String username) {
+        return usernameToUserData.getOrDefault(username, null);
+    }
+
+    void createUser(UserData userData) {
+        setOfUsers.add(userData);
+        usernameToUserData.put(userData.username(), userData);
+    }
+
+    void clear() {
+        setOfUsers.clear();
+        usernameToUserData.clear();
+    }
 }
