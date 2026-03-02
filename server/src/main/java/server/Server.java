52ce39329da101c46cf9945e1c2f38efbaa7e692
blazzer <blazzer@students.byu.edu>
2026-03-02 12:26:01 -0700

    Added endpoints in ServerMain

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,18 +6,22 @@ public class Server {
 
     private final Javalin javalin;
     private ClearHandler clearer = new ClearHandler();
-    private CreateGameHandler gameCreater = new CreateGameHandler();
+    private final CreateGameHandler gameCreater = new CreateGameHandler();
     private JoinGameHandler gameJoiner = new JoinGameHandler();
-    private ListGamesHandler lister = new ListGamesHandler();
-    private LoginHandler loggerIn = new LoginHandler();
-    private LogoutHandler loggerOut = new LogoutHandler();
-    private RegisterHandler registerer = new RegisterHandler();
+    private final ListGamesHandler lister = new ListGamesHandler();
+    private final LoginHandler loggerIn = new LoginHandler();
+    private final LogoutHandler loggerOut = new LogoutHandler();
+    private final RegisterHandler registerer = new RegisterHandler();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-
-        // Register your endpoints and exception handlers here.
+        javalin.post("/user", registerer);
+        javalin.post("/session", loggerIn);
+        javalin.delete("/session", loggerOut);
+        javalin.get("/game", lister);
+        javalin.post("/game", gameCreater);
+        javalin.put("/game", gameJoiner);
+        javalin.delete("/db", clearer);
 
     }
 
