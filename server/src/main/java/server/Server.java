0883d0ebff4bc7e01cb9a1e1332077da3c1275a8
blazzer <blazzer@students.byu.edu>
2026-03-01 20:32:53 -0700

    Created model record classes and added handler instance variables to Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,9 +5,17 @@ import io.javalin.*;
 public class Server {
 
     private final Javalin javalin;
+    private ClearHandler clearer = new ClearHandler();
+    private CreateGameHandler gameCreater = new CreateGameHandler();
+    private JoinGameHandler gameJoiner = new JoinGameHandler();
+    private ListGamesHandler lister = new ListGamesHandler();
+    private LoginHandler loggerIn = new LoginHandler();
+    private LogoutHandler loggerOut = new LogoutHandler();
+    private RegisterHandler registerer = new RegisterHandler();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
 
         // Register your endpoints and exception handlers here.
 
