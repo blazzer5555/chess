738c4426b74206db7faccb19ee356c48c89b7292
blazzer <blazzer@students.byu.edu>
2026-03-20 18:57:47 -0600

    Implemented getUser() in DatabaseUserDAO

--- server/src/main/java/dataaccess/DatabaseAuthDAO.java
+++ server/src/main/java/dataaccess/DatabaseAuthDAO.java
@@ -1,13 +1,12 @@
 package dataaccess;
 
 import model.AuthData;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.gson.Gson;
+import server.LoginRequest;
 
 public class DatabaseAuthDAO {
 
-    private static final String[] deletionStatements = {
+    private static final String[] clearStatements = {
             "DELETE FROM authtoken",
             "DELETE FROM authdata"
         };
@@ -17,15 +16,44 @@ public class DatabaseAuthDAO {
     }
 
     public void addAuth(AuthData authData) {
+        Gson gson = new Gson();
+        String serializedAuthData = gson.toJson(authData);
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chessdatabase");
+            String[] insertStatements = {
+                    "INSERT INTO authdata(authdata) values (" + serializedAuthData + ");",
+                    "INSERT INTO authtoken(authtoken) values (" + authData.authToken() + ");"
+            };
+            for (String statement: insertStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Could not add AuthData to the database.");
+        }
     }
 
     public void deleteAuth(AuthData authData) {
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chessdatabase");
+            String[] deleteStatements = {};
+            for (String statement: deleteStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Could not delete AuthData from the database.");
+        }
     }
 
     public void clear() {
         try (var conn = DatabaseManager.getConnection()) {
             conn.setCatalog("chessdatabase");
-            for (String statement: deletionStatements) {
+            for (String statement: clearStatements) {
                 try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
--- server/src/main/java/dataaccess/DatabaseUserDAO.java
+++ server/src/main/java/dataaccess/DatabaseUserDAO.java
@@ -1,31 +1,67 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.UserData;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.sql.ResultSet;
 
 public class DatabaseUserDAO {
 
-    private static final String[] deletionStatements = {
+    private static final String[] clearStatements = {
             "DELETE FROM username",
             "DELETE FROM userdata"
         };
 
     public UserData getUser(String username) {
+        Gson gson = new Gson();
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chessdatabase");
+            String getStatement1 = "SELECT userid FROM username WHERE username = \"" + username + "\"";
+            try (var preparedStatement = conn.prepareStatement(getStatement1)) {
+                ResultSet rs = preparedStatement.executeQuery();
+                try {
+                    int id = rs.getInt(1);
+                    String getStatement2 = "SELECT userdata FROM userdata WHERE id = \"" + id + "\"";
+                    try (var preparedStatement2 = conn.prepareStatement(getStatement2)) {
+                        String serializedUserData = String.valueOf(preparedStatement2.executeQuery());
+                        return gson.fromJson(serializedUserData, UserData.class);
+                    }
+                }
+                catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Could not get UserData from the database.");
+        }
         return null;
     }
 
     public void createUser(UserData userData) {
-        return;
+        Gson gson = new Gson();
+        String serializedUserData = gson.toJson(userData);
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chessdatabase");
+            String[] createStatements = {
+                    "INSERT INTO userdata(userdata) values (" + serializedUserData + ");",
+                    "INSERT INTO username(username) values (" + userData.username() + ");"
+            };
+            for (String statement: createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Could not add UserData to the database.");
+        }
     }
 
     public void clear() {
         try (var conn = DatabaseManager.getConnection()) {
             conn.setCatalog("chessdatabase");
-            for (String statement: deletionStatements) {
+            for (String statement: clearStatements) {
                 try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
