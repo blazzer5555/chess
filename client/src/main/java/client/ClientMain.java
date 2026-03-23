48dfd6952e7ef2b7e6954d8b8572623f70b3dc2b
blazzer <blazzer@students.byu.edu>
2026-03-23 08:34:40 -0600

    Created pre login REPL with no server functionality

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,55 @@ package client;
 
 import chess.*;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Did someone ask to play chess?");
+        boolean doneWithProgram = false;
+        while (!doneWithProgram) {
+            System.out.println("Type the number associated with the action, then press enter. \n");
+            System.out.println("1. Help\n2. Quit\n3. Log in\n4. Register");
+            int userResponse = scanner.nextInt();
+            switch (userResponse) {
+                case (1):
+                    System.out.println("Quit: Exit the program.");
+                    System.out.println("Log in: Log in to an existing chess account.");
+                    System.out.println("Register: Register a new chess account.");
+                    break;
+                case (2):
+                    doneWithProgram = true;
+                    break;
+                case (3):
+                    System.out.println("Please enter your username.");
+                    String loginUsername = scanner.nextLine();
+                    loginUsername = scanner.nextLine();
+                    System.out.println("Please enter your password.");
+                    String loginPassword = scanner.nextLine();
+                    break;
+                case (4):
+                    System.out.println("Please enter a username.");
+                    String registerUsername = scanner.nextLine();
+                    registerUsername = scanner.nextLine();
+                    boolean userConfirmedPassword = false;
+                    while (!userConfirmedPassword) {
+                        userConfirmedPassword = true;
+                        System.out.println("Please enter a password.");
+                        String registerPassword = scanner.nextLine();
+                        System.out.println("Please confirm your password.");
+                        String confirmPassword = scanner.nextLine();
+                        if (!Objects.equals(registerPassword, confirmPassword)) {
+                            System.out.println("Passwords do not match, please retry.");
+                            userConfirmedPassword = false;
+                        }
+                    }
+                    System.out.println("Please enter the email you'd like to use with this account");
+                    String registerEmail = scanner.nextLine();
+                    break;
+            }
+        }
+        System.out.println("Thanks for playing!");
     }
 }
