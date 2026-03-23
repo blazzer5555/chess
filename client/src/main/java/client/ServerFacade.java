c47caf9e3eec78f26b680d06087b55d404e4dc78
blazzer <blazzer@students.byu.edu>
2026-03-23 09:22:21 -0600

    Created blueprint for ServerFacade class

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,34 @@
+package client;
+
+import model.*;
+
+public class ServerFacade {
+
+    public void sendRegisterRequest(UserData registerRequest) {
+
+    }
+
+    public void sendLoginRequest(LoginRequest loginRequest) {
+
+    }
+
+    public void sendLogoutRequest(AuthData logoutRequest) {
+
+    }
+
+    public void sendCreateGameRequest(CreateGameRequest createGameRequest) {
+
+    }
+
+    public void sendJoinGameRequest(JoinGameRequest joinGameRequest) {
+
+    }
+
+    public void sendListGamesRequest() {
+
+    }
+
+    public void sendClearRequest() {
+
+    }
+}
