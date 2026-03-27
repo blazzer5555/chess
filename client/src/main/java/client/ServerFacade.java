196773225a9476298a9fd86092bcf27aa984b298
blazzer <blazzer@students.byu.edu>
2026-03-27 15:20:14 -0600

    Implemented server facade methods

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,34 +1,38 @@
 package client;
 
 import model.*;
+import java.util.ArrayList;
 
 public class ServerFacade {
 
+    public static final ServerCommunicator communicator = new ServerCommunicator();
+
     public void sendRegisterRequest(UserData registerRequest) {
-
+        communicator.sendRegisterRequest(registerRequest);
     }
 
-    public void sendLoginRequest(LoginRequest loginRequest) {
-
+    public String sendLoginRequest(LoginRequest loginRequest) {
+        communicator.sendLoginRequest(loginRequest);
+        return "";
     }
 
-    public void sendLogoutRequest(AuthData logoutRequest) {
-
+    public void sendLogoutRequest(String authToken) {
+        communicator.sendLogoutRequest(authToken);
     }
 
     public void sendCreateGameRequest(CreateGameRequest createGameRequest) {
-
+        communicator.sendCreateGameRequest(createGameRequest);
     }
 
     public void sendJoinGameRequest(JoinGameRequest joinGameRequest) {
-
+        communicator.sendJoinGameRequest(joinGameRequest);
     }
 
-    public void sendListGamesRequest() {
-
+    public ArrayList<ListGamesReturnData> sendListGamesRequest() {
+        return communicator.sendListGamesRequest();
     }
 
     public void sendClearRequest() {
-
+        communicator.sendClearRequest();
     }
 }
