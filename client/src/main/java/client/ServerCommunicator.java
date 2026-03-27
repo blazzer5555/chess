42fb34cc8c5715e706181a2343db55da3137aaa0
blazzer <blazzer@students.byu.edu>
2026-03-27 13:01:56 -0600

    Created outline for SevrerCommunicator class

---
+++ client/src/main/java/client/ServerCommunicator.java
@@ -0,0 +1,36 @@
+package client;
+
+import model.*;
+
+import java.util.ArrayList;
+
+public class ServerCommunicator {
+
+    public void sendRegisterRequest(UserData registerRequest) {
+
+    }
+
+    public String sendLoginRequest(LoginRequest loginRequest) {
+        return null;
+    }
+
+    public void sendLogoutRequest(String authToken) {
+
+    }
+
+    public void sendCreateGameRequest(CreateGameRequest createGameRequest) {
+
+    }
+
+    public void sendJoinGameRequest(JoinGameRequest joinGameRequest) {
+
+    }
+
+    public ArrayList<ListGamesReturnData> sendListGamesRequest() {
+        return null;
+    }
+
+    public void sendClearRequest() {
+
+    }
+}
