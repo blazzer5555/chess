09b292a50be65fe9d27a1398d40c1288529e91ba
blazzer <blazzer@students.byu.edu>
2026-03-27 15:48:02 -0600

    Implemented listGamesRequest and RegisterRequest in Server Communicator

--- client/src/main/java/client/ServerCommunicator.java
+++ client/src/main/java/client/ServerCommunicator.java
@@ -1,36 +1,74 @@
 package client;
 
+import com.google.gson.Gson;
 import model.*;
-
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ServerCommunicator {
 
-    public void sendRegisterRequest(UserData registerRequest) {
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final Gson gson = new Gson();
 
-    }
-
-    public String sendLoginRequest(LoginRequest loginRequest) {
+    public String sendRegisterRequest(UserData registerRequest) throws Exception {
+        String urlString = "http://localhost:8080/user";
+        String registerBody = gson.toJson(registerRequest);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(urlString))
+                .timeout(java.time.Duration.ofMillis(5000))
+                .POST(HttpRequest.BodyPublishers.ofString(registerBody))
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        if (httpResponse.statusCode() >= 200 && httpResponse.statusCode() < 300) {
+            return gson.fromJson(httpResponse.body(), AuthData.class).authToken();
+        } else {
+            System.out.println("Error: received status code " + httpResponse.statusCode());
+        }
         return null;
     }
 
-    public void sendLogoutRequest(String authToken) {
-
-    }
-
-    public void sendCreateGameRequest(CreateGameRequest createGameRequest) {
-
-    }
-
-    public void sendJoinGameRequest(JoinGameRequest joinGameRequest) {
-
-    }
-
-    public ArrayList<ListGamesReturnData> sendListGamesRequest() {
+    public String sendLoginRequest(LoginRequest loginRequest) throws Exception {
         return null;
     }
 
-    public void sendClearRequest() {
+    public void sendLogoutRequest(String authToken) throws Exception {
+
+    }
+
+    public int sendCreateGameRequest(CreateGameRequest createGameRequest) throws Exception {
+        return 0;
+    }
+
+    public void sendJoinGameRequest(JoinGameRequest joinGameRequest) throws Exception {
+
+    }
+
+    public ArrayList<ListGamesReturnData> sendListGamesRequest() throws Exception {
+        String urlString = "http://localhost:8080/game";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(urlString))
+                .timeout(java.time.Duration.ofMillis(5000))
+                .GET()
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        record ListResponse(ArrayList<ListGamesReturnData> games) {
+        }
+        if (httpResponse.statusCode() >= 200 && httpResponse.statusCode() < 300) {
+            ListResponse returnList = gson.fromJson(httpResponse.body(), ListResponse.class);
+            return returnList.games();
+        } else {
+            System.out.println("Error: received status code " + httpResponse.statusCode());
+        }
+        return null;
+    }
+
+    public void sendClearRequest() throws Exception {
 
     }
 }
